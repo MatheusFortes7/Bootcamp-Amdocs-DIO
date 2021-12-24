0571324a801e6522683e6f283d65e1b08aa109bb
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2021-12-24 15:48:47 -0300

    Add files via upload

---
+++ Exercicio2IOScanner.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Exercicio2IOScanner {
+    public static void lerTecladoEscreverDocumento() throws IOException {
+        System.out.println("Digite 3 recomendacoes de filmes: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        File f = new File("recomendacoes.txt");
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            line = scanner.nextLine();
+        } while (!(line.equalsIgnoreCase("FIM")));
+        System.out.println("Arquivo criado com sucesso! " + f.getName());
+
+        bw.close();
+        scanner.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        lerTecladoEscreverDocumento();
+    }
+}
