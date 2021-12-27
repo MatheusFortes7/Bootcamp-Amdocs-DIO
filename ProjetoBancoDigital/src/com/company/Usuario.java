11af351d152e360928ef341511c6b251ef8e93de
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2021-12-27 19:15:02 -0300

    add class Usuario

--- ProjetoBancoDigital/src/com/company/Usuario.java
+++ ProjetoBancoDigital/src/com/company/Usuario.java
@@ -4,13 +4,9 @@ public class Usuario {
 
     public String nome;
 
-    public Usuario(String nome) {
-        this.nome = nome;
-    }
+    public Usuario(String nome) { this.nome = nome; }
 
-    public String getNome() {
-        return nome;
-    }
+    public String getNome() { return nome; }
 
     public void setNome(String nome) {
         this.nome = nome;
